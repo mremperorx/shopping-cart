4e7e1f56e4d29d0b92c3fb8a56632452c7aeb364
mremperorx <Majedalnasr7@gmail.com>
2022-11-14 02:07:36 +0200

    Add Link to Logo.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,6 @@ import { motion } from "framer-motion";
 
 export function Home() {
     return (
-
         <motion.div initial={{ width: 0 }} animate={{ width: "100%" }} exit={{ x: window.innerWidth, transition: { duration: 0.5 } }}>
             <h1>Home</h1>
             <ImageSlider />
